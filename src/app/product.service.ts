df02f93f7ee7fcfff1c127d3969b28d93e196efd
rudrajyotibiswas <rudrajyoti.biswas@outlook.com>
2020-04-12 00:01:24 +0530

    part 3: iteration 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import './track';
+import 'rxjs/Observable';
+import {Album} from './album';
 
 @Injectable()
 export class ProductService {
@@ -11,7 +14,7 @@ export class ProductService {
 
   getAlbum(id:number)
   {
-   return  this._http.get(this._albumUrl).map((response)=>response.json());
+   return  this._http.get(this._albumUrl).map((response)=><Album>response.json());
 
   }
 
