10c234c1ebb51dd797a83f2753e60c98b6d5a2b7
rudrajyotibiswas <rudrajyoti.biswas@outlook.com>
2020-04-12 00:45:31 +0530

    part 6 : iteration 1

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,11 +5,13 @@ import './track';
 import 'rxjs/Observable';
 import {Album} from './album';
 import { Observable } from 'rxjs/Observable';
+import {Product} from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = "../assets/album.json";
+  private _productsUrl = "../assets/products.json";
 
   constructor(private _http: Http) { }
 
@@ -17,6 +19,11 @@ export class ProductService {
   {
    return  this._http.get(this._albumUrl).map((response)=><Album>response.json());
 
+  }
+
+  getProducts():Observable<Product[]>
+  {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 
 }
